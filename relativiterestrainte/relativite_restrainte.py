3f0f8ec93aee88efd193aa24ca1d64381ea85010
BegaudMatisse <matisse.begaud@stfelixlasalle.fr>
2026-04-07 08:59:52 +0200

    Update relativite_restrainte.py

--- relativiterestrainte/relativite_restrainte.py
+++ relativiterestrainte/relativite_restrainte.py
@@ -1,7 +1,21 @@
 import sys
 import math
 import random
+from turtle import width
 import pygame
 
-class RelativiteRestreinte:
-    
+pygame.init()
+
+window = pygame.display.set_mode((1100, 900))
+pygame.display.set_caption('pygame_window')
+
+run = True
+while run:
+    pygame.time.delay(60)
+    print("test")
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #pour quitter le jeu
+            run = False
+
+pygame.quit()
