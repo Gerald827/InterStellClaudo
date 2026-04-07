e7a33f95848c554efa4cd0199a0428a72d50e836
BegaudMatisse <matisse.begaud@stfelixlasalle.fr>
2026-04-07 09:04:38 +0200

    Update relativite_restrainte.py

--- relativiterestrainte/relativite_restrainte.py
+++ relativiterestrainte/relativite_restrainte.py
@@ -9,10 +9,9 @@ pygame.init()
 window = pygame.display.set_mode((1100, 900))
 pygame.display.set_caption('pygame_window')
 
-run = False
+run = True
 while run:
     pygame.time.delay(60)
-    print("test")
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #pour quitter le jeu
