8e6275ca473a270383e5c0ca70ea54fe918f5378
BegaudMatisse <matisse.begaud@stfelixlasalle.fr>
2026-04-07 09:25:25 +0200

    Update relativite_restrainte.py

--- relativiterestrainte/relativite_restrainte.py
+++ relativiterestrainte/relativite_restrainte.py
@@ -2,16 +2,48 @@ import sys
 import math
 import random
 from turtle import width
+# Source - https://stackoverflow.com/a/59944869
+# Posted by Rabbid76, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-04-07, License - CC BY-SA 4.0
+
 import pygame
 
 pygame.init()
+window = pygame.display.set_mode((200, 200))
+clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 100)
 
-window = pygame.display.set_mode((1100, 900))
-pygame.display.set_caption('pygame_window')
+counter = 0
+text = font.render(str(counter), True, (0, 128, 0))
 
+time_delay = 1000
+timer_event = pygame.USEREVENT+1
+pygame.time.set_timer(timer_event, time_delay)
+
+# main application loop
 run = True
 while run:
-    pygame.time.delay(60)
+    clock.tick(60)
+
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        elif event.type == timer_event:
+            # recreate text
+            counter += 1
+            text = font.render(str(counter), True, (0, 128, 0))
+
+    # clear the display
+    window.fill((255, 255, 255))
+
+    # draw the scene
+    text_rect = text.get_rect(center = window.get_rect().center)   
+    window.blit(text, text_rect)
+
+    # update the display
+    pygame.display.flip()
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #pour quitter le jeu
