f02eccc47b1228a1efcdd8c571711c143648fec3
kenzoaudaire85 <kenzo.audaire@stfelixlasalle.fr>
2026-04-28 08:45:17 +0200

    Update relativite_restrainte.py

--- relativiterestrainte/relativite_restrainte.py
+++ relativiterestrainte/relativite_restrainte.py
@@ -26,11 +26,16 @@ font = pygame.font.SysFont(None, 100)
 celerite = 299792458  # m/s aka vitesse de la lumière
 
 counter = 0
+counter2 = 0
 text = font.render(str(counter), True, (RED))
+text2 = font.render(str(counter2), True, (GREEN))
 
 time_delay = 1000
+timer_delay_2 = 500
 timer_event = pygame.USEREVENT + 1
+timer_event2 = pygame.USEREVENT + 2
 pygame.time.set_timer(timer_event, time_delay)
+pygame.time.set_timer(timer_event2, timer_delay_2)
 
 stars = [(random.randint(0, 900), random.randint(0, 900)) for _ in range(150)]
 
@@ -45,6 +50,9 @@ while run:
         elif event.type == timer_event:
             counter += 1
             text = font.render(str(counter), True, (RED))
+        elif event.type == timer_event2:
+            counter2 += 1
+            text2 = font.render(str(counter2), True, (GREEN))
             
     window.fill((0, 0, 0))
     #étoils des années 39 - 45
@@ -52,10 +60,12 @@ while run:
         pygame.draw.circle(window, (255, 255, 255), star, 2)
 
     # afficher le compteur
-    text_rect = text.get_rect(center=window.get_rect().center)
+    text_rect = text.get_rect(center=(text.get_width() // 2, window.get_height() // 2))
     window.blit(text, text_rect)
     #jusqu'a la
 
+    text_rect2 = text2.get_rect(center=(window.get_width() - text2.get_width() // 2, window.get_height() // 2))
+    window.blit(text2, text_rect2)
     pygame.display.flip()#flip flop
 
 pygame.quit()#Quitter
